ce25479b805dd122da44804078bdcd4300756299
Juju <juju@ribon.io>
2024-03-15 13:29:38 -0300

    feat: create a mock sign up page

--- src/Pages/Auth/SignUp/index.tsx
+++ src/Pages/Auth/SignUp/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Button, Text, TextInput, View } from "react-native";
 
-export default function SignIn() {
+export default function SignUp() {
   return (
     <View>
-      <Text>Sign In</Text>
+      <Text>Sign Up</Text>
+      <Text>Name</Text>
+      <TextInput />
       <Text>E-mail</Text>
       <TextInput />
       <Text>Password</Text>
+      <TextInput />
+      <Text>Confirm Password</Text>
+      <TextInput />
+      <Text>Selfie</Text>
       <TextInput />
       <Button title="Confirm" />
     </View>
