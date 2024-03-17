1f01054defb0c842ab1e886e22137c2f0dbd163a
Juju <juju@ribon.io>
2024-03-17 11:55:18 -0300

    feat: add image picker

--- src/screens/Auth/SignUp/index.tsx
+++ src/screens/Auth/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, View, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import { useAuth } from "../../../contexts/AuthContext";
 import InputWithLabel from "../../../components/InputWithLabel";
 
@@ -11,6 +12,22 @@ export default function SignUp() {
   const [selfie, setSelfie] = useState("");
 
   const { signUp } = useAuth();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setSelfie(result.assets[0].uri);
+    }
+  };
+
   return (
     <View>
       <InputWithLabel
@@ -37,13 +54,13 @@ export default function SignUp() {
         onChangeText={(value) => setConfirmPassword(value)}
         placeholder={""}
       />
-      <InputWithLabel
-        label="Selfie"
-        value={selfie}
-        onChangeText={(value) => setSelfie(value)}
-        placeholder={""}
-      />
-
+      <Button title="Upload a selfie" onPress={pickImage} />
+      {selfie && (
+        <Image
+          source={{ uri: selfie }}
+          style={{ width: 100, height: 100, borderRadius: 100 }}
+        />
+      )}
       <Button
         title="Confirm"
         onPress={() => signUp(name, email, password, selfie)}
